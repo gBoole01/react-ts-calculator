faaff843e615fc3183c697c7718a3025552a5a98
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-12 11:58:34 +0100

    feat: add dispatch call and discrimination union type for actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Reducer, useReducer } from 'react'
 import { DigitButton } from './DigitButton'
+import { OperationButton } from './OperationButton'
 
 export enum Actions {
   ADD_DIGIT = 'add-digit',
@@ -9,10 +10,12 @@ export enum Actions {
   EVALUATE = 'evaluate',
 }
 
-export type ReducerAction = {
-  type: Actions
-  payload: { digit: string }
-}
+export type ReducerAction =
+  | { type: Actions.ADD_DIGIT; payload: { digit: string } }
+  | { type: Actions.CHOOSE_OPERATION; payload: { symbol: string } }
+  | { type: Actions.CLEAR }
+  | { type: Actions.DELETE_DIGIT }
+  | { type: Actions.EVALUATE }
 
 type ReducerState = {
   currentOperand: string
@@ -21,21 +24,23 @@ type ReducerState = {
 }
 
 function reducer(state: ReducerState, action: ReducerAction): ReducerState {
-  const { type, payload } = action
-
-  switch (type) {
+  switch (action.type) {
     case Actions.ADD_DIGIT:
       return {
         ...state,
-        currentOperand: `${state.currentOperand || ''}${payload.digit}`,
+        currentOperand: `${state.currentOperand || ''}${action.payload.digit}`,
       }
     case Actions.CHOOSE_OPERATION:
+      console.log(action)
       return state
     case Actions.CLEAR:
+      console.log(action)
       return state
     case Actions.DELETE_DIGIT:
+      console.log(action)
       return state
     case Actions.EVALUATE:
+      console.log(action)
       return state
     default:
       return state
@@ -55,24 +60,36 @@ function App() {
         </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: Actions.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: Actions.DELETE_DIGIT })}>
+        DEL
+      </button>
+      <OperationButton dispatch={dispatch} symbol="÷" />
       <DigitButton dispatch={dispatch} digit="1" />
       <DigitButton dispatch={dispatch} digit="2" />
       <DigitButton dispatch={dispatch} digit="3" />
-      <button>*</button>
+      <OperationButton dispatch={dispatch} symbol="*" />
       <DigitButton dispatch={dispatch} digit="4" />
       <DigitButton dispatch={dispatch} digit="5" />
       <DigitButton dispatch={dispatch} digit="6" />
-      <button>+</button>
+      <OperationButton dispatch={dispatch} symbol="+" />
       <DigitButton dispatch={dispatch} digit="7" />
       <DigitButton dispatch={dispatch} digit="8" />
       <DigitButton dispatch={dispatch} digit="9" />
-      <button>-</button>
-      <button>.</button>
+      <OperationButton dispatch={dispatch} symbol="-" />
+      <DigitButton dispatch={dispatch} digit="." />
       <DigitButton dispatch={dispatch} digit="0" />
-      <button className="span-two">=</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: Actions.EVALUATE })}
+      >
+        =
+      </button>
     </div>
   )
 }
